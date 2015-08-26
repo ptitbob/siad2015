eb9a6f830dd09a509b7c5b25627dec24df909e90
ptitbob <github@shipstone.org>
2015-08-26 23:58:58 +0200

    initialisation progression cours

---
+++ 00-jsf-intro/src/main/java/fr/univ/tours/siad/jsf/intro/MyApplication.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.univ.tours.siad.jsf.intro;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author robert-f
+ */
+@ApplicationPath("")
+public class MyApplication extends Application {
+}
