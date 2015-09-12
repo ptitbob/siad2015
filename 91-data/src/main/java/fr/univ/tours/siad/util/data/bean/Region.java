f1492ff6fdabc060a383562e5190e1e5289294f0
ptitbob <github@shipstone.org>
2015-09-12 02:13:59 +0200

    Ajout lien region / departement

--- 91-data/src/main/java/fr/univ/tours/siad/util/data/bean/Region.java
+++ 91-data/src/main/java/fr/univ/tours/siad/util/data/bean/Region.java
@@ -2,16 +2,22 @@ package fr.univ.tours.siad.util.data.bean;
 
 import javax.persistence.*;
 import java.util.Objects;
+import java.util.Set;
+
+import static javax.persistence.FetchType.LAZY;
 
 @Entity
 @SequenceGenerator(name = "region_sequence", sequenceName = "region_sequence", allocationSize = 1)
 @NamedQueries({
         @NamedQuery(name = Region.FIND_ALL, query = "select r from Region r")
         , @NamedQuery(name = Region.COUNT, query = "select count(r) from Region r")
+        , @NamedQuery(name = Region.FIND_BY_INSEEID, query = "select r from Region r where r.inseeId = :" + Region.INSEEID)
 })
 public class Region {
     public static final String FIND_ALL = "Region.FIND_ALL";
     public static final String COUNT = "Region.COUNT";
+    public static final String INSEEID = "INSEEID";
+    public static final String FIND_BY_INSEEID = "Region.FIND_BY_INSEEID";
 
     @Id @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "region_sequence")
     @Column(name = "region_id")
@@ -28,6 +34,9 @@ public class Region {
 
     @Column(length = 100)
     private String name;
+
+    @OneToMany(fetch = LAZY, mappedBy = "region")
+    private Set<District> districtSet;
 
     public Region() {
     }
@@ -77,6 +86,14 @@ public class Region {
 
     public void setUpperName(String upperName) {
         this.upperName = upperName;
+    }
+
+    public Set<District> getDistrictSet() {
+        return districtSet;
+    }
+
+    public void setDistrictSet(Set<District> districtSet) {
+        this.districtSet = districtSet;
     }
 
     @Override
