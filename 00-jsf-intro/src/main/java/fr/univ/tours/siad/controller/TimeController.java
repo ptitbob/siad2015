389314c4f49fd446e26a53b1d5a951f66379d1a4
ptitbob <github@shipstone.org>
2015-09-20 20:37:22 +0200

    Ajout de la serialisation

--- 00-jsf-intro/src/main/java/fr/univ/tours/siad/controller/TimeController.java
+++ 00-jsf-intro/src/main/java/fr/univ/tours/siad/controller/TimeController.java
@@ -2,11 +2,14 @@ package fr.univ.tours.siad.controller;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
+import java.io.Serializable;
 import java.util.Date;
 
 @Named
 @RequestScoped
-public class TimeController {
+public class TimeController implements Serializable {
+    
+    private static final long serialVersionUID = -7748708046976719284L;
 
     private Date date = new Date();
 
