24e45f42194bd17e263644d2cd8835f0c3d96c04
ptitbob <github@shipstone.org>
2015-11-04 17:19:48 +0100

    Ajout exercice 1

---
+++ 31-jaxrs-td1/src/main/java/fr/univ/tours/siad/ws/rs/SiadRestAPI.java
@@ -0,0 +1,8 @@
+package fr.univ.tours.siad.ws.rs;
+
+/**
+ * Classe de base de l'API REST du projet JAX-RS
+ * Completer la classe afin d'initialisé (en termes JavaEE l'API REST du projet)
+ */
+public class SiadRestAPI {
+}
